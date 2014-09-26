058bd0e601a9e297efaf1f317a4829f1fa3c601d
x00092816 <x00092816@ittd.ie>
2014-09-26 15:43:44 +0100

    converted the file to a bash script executional

--- count.sh
+++ count.sh
@@ -1,2 +1,12 @@
+#!/bin/bash
+
+# Title : count.sh
+# Date : 26/09/2014
+# Author : x00092816@ittd.ie
+# Version :0
+# Description : CPU count script
+# Options : 
+
+
 grep processor /proc/cpuinfo | wc -l
 
